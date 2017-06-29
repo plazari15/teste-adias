5719108ff6c6163c2b0681ddfbf14cd3d550a6ca
Pedro <plazari96@gmai.com>
2017-06-28 22:04:40 -0300

    inserindo o HTML, Máscara jQuery e Preparando Form

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -1,1 +1,12 @@
-principal.js
+$(document).ready(function(){
+    $(function() {
+        $('.currency').maskMoney();
+    });
+
+    $('#formCalculadora').submit(function () {
+       alert('fará o cálculo');
+
+       return false;
+    });
+});
+
